05428fb6e70fca6927d32048186033ff96f50b87
lukitasmois <lmoix85@gmail.com>
2024-06-10 15:33:17 -0300

    cambio en App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/home/home';
+import Login from './components/login/login';
+import Libros from './components/libros/libros';
+import BuscarLibros from './components/libros/buscarLibros';
+import Inventario from './components/sistema/inventario';
+import AniadirLibro from './components/sistema/aniadirLibro';
+import DescartarLibro from './components/sistema/descartarLibro';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="aplicacion">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/libros" element={<Libros />} />
+          <Route path="/buscarLibros" element={<BuscarLibros/>} />
+          <Route path="/buscarLibros" element={<BuscarLibros/>} />
+          <Route path="/inventario" element={<Inventario/>} />
+          <Route path="/aniadirLibro" element={<AniadirLibro/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
+    
   );
 }
 
---
+++ src/components/sistema/descartarLibro.jsx
@@ -0,0 +1,9 @@
+const DescartarLibro = () => {
+    return (
+        <div>
+            <h1>DESCARTAR LIBRO</h1>
+        </div>
+    );
+}
+
+export default DescartarLibro;
